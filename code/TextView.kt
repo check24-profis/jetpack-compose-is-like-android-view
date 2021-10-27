fecba8ad474c07313bbdc02cd7dcf5e658ee710d
Fabian Jung <fabian.jung@check24.de>
2021-10-27 11:27:39 +0200

    made text in compose and xml the same

--- code/TextView.kt
+++ code/TextView.kt
@@ -1,4 +1,12 @@
 @Composable
 fun Label() {
-    Text("Hello World")
+    Text(
+        text = "Hello World",
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier
+            .wrapContentHeight()
+            .wrapContentWidth(),
+        style = TextStyle(background = Color(0xFF4a8fff))
+    )
 }
