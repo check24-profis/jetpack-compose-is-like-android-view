bdebb21b8750099956c0b8cda9c14e9b99aef935
Florian Stallmach <florian.stallmach@check24.com>
2022-03-02 15:53:41 +0100

    add phone number

---
+++ code/text/TextViewPhoneNumber.kt
@@ -0,0 +1,17 @@
+@Composable
+fun PhoneNumber() {
+    var text by remember { mutableStateOf(TextFieldValue("012345678900")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        maxLines = 1,
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight(),
+        label = { Text(text = "Your Label") },
+        placeholder = { Text(text = "Your Placeholder/Hint") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+    )
+}
