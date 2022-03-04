f493b316dae46aee163a2c6115d9ff93abf49deb
Florian Stallmach <florian.stallmach@check24.com>
2022-03-04 17:56:05 +0100

    organize imports

--- demo/src/main/java/de/check24/demo/features/text/AndroidUITextActivity.kt
+++ demo/src/main/java/de/check24/demo/features/text/AndroidUITextActivity.kt
@@ -1,8 +1,6 @@
 package de.check24.demo.features.text
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import de.check24.demo.R
 
