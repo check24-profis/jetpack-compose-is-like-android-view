0d7c49d841557d0a2ba003dcb02e7dd2451ff2a2
Jan Rabe <jan.rabe@check24.de>
2022-03-04 11:08:18 +0100

    added menu

--- demo/src/main/java/de/check24/demo/MainActivity.kt
+++ demo/src/main/java/de/check24/demo/MainActivity.kt
@@ -1,28 +1,36 @@
 package de.check24.demo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_6
 import androidx.compose.ui.tooling.preview.Preview
+import de.check24.demo.extensions.findActivity
+import de.check24.demo.features.text.TextActivity
 import de.check24.demo.ui.theme.DemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Menu()
                 }
             }
         }
@@ -30,14 +38,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun Menu() {
+
+    val activity = LocalContext.current.findActivity() ?: return
+
+    Column {
+
+        Row {
+            Text(text = "Text", modifier = Modifier
+                .clickable {
+                    activity.startActivity(Intent(activity, TextActivity::class.java))
+                }
+            )
+        }
+
+        Row {
+            Text(text = "Password", modifier = Modifier
+                .clickable {
+                    activity.startActivity(Intent(activity, TextActivity::class.java))
+                }
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = NEXUS_6, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     DemoTheme {
-        Greeting("Android")
+        Menu()
     }
 }
---
+++ demo/src/main/java/de/check24/demo/features/text/TextActivity.kt
@@ -0,0 +1,44 @@
+package de.check24.demo.features.text
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import de.check24.demo.ui.theme.DemoTheme
+
+class TextActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    DemoTheme {
+        Greeting("Android")
+    }
+}
