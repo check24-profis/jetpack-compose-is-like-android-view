71bb7925b219c7f6fce69223d972ff5ac129133b
Darius Mertin <darius.mertin@check24.de>
2022-03-07 16:33:13 +0100

    added activity

---
+++ demo/src/main/java/de/check24/demo/features/custom/AndroidUIGoogleMapActivity.kt
@@ -0,0 +1,11 @@
+package de.check24.demo.features.custom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AndroidUIGoogleMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
