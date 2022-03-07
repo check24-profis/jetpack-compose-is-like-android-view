875226f9b4bd2708ae914190a581852858cdbf58
Justus Saringer <justus.saringer@check24.com>
2022-03-07 17:30:17 +0100

    Comments adopted

--- demo/src/main/java/de/check24/demo/features/switch/ComposableSwitchActivity.kt
+++ demo/src/main/java/de/check24/demo/features/switch/ComposableSwitchActivity.kt
@@ -1,21 +1,31 @@
 package de.check24.demo.features.switch
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import de.check24.demo.features.constraintlayout.barrier.ConstraintLayoutBarrier
 import de.check24.demo.ui.theme.DemoTheme
 
-class ComposableSwitchActivity : AppCompatActivity() {
+class ComposableSwitchActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +35,16 @@ class ComposableSwitchActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SwitchComposable()
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "Switch")
+                                })
+                        }, content = {
+                            Switch()
+                        }
+                    )
                 }
             }
         }
@@ -33,18 +52,25 @@ class ComposableSwitchActivity : AppCompatActivity() {
 }
 
 @Composable
-private fun SwitchComposable() {
-    val checkedState = remember { mutableStateOf(false) }
-    Switch(
-        checked = checkedState.value,
-        onCheckedChange = { checkedState.value = it }
-    )
+private fun Switch() {
+
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Arrangement.CenterHorizontally
+    ) {
+        val checkedState = remember { mutableStateOf(false) }
+        Switch(
+            checked = checkedState.value,
+            onCheckedChange = { checkedState.value = it }
+        )
+    }
 }
 
 @Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
 @Composable
-private fun DatePreview() {
+private fun SwitchPreview() {
     DemoTheme {
-        SwitchComposable()
+        Switch()
     }
 }
