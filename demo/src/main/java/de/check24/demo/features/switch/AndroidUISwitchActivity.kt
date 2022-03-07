5369622988be943e93e744e62fedf4c3a05302f1
Justus Saringer <justus.saringer@check24.com>
2022-03-07 15:58:31 +0100

    added switches to UI and Compose in the app

---
+++ demo/src/main/java/de/check24/demo/features/switch/AndroidUISwitchActivity.kt
@@ -0,0 +1,13 @@
+package de.check24.demo.features.switch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import de.check24.demo.R
+
+class AndroidUISwitchActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.uiswitch)
+    }
+}
---
+++ demo/src/main/java/de/check24/demo/features/switch/ComposableSwitchActivity.kt
@@ -0,0 +1,50 @@
+package de.check24.demo.features.switch
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import de.check24.demo.ui.theme.DemoTheme
+
+class ComposableSwitchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    SwitchComposable()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SwitchComposable() {
+    val checkedState = remember { mutableStateOf(true) }
+    Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
+    )
+}
+
+@Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
+@Composable
+private fun DatePreview() {
+    DemoTheme {
+        SwitchComposable()
+    }
+}
