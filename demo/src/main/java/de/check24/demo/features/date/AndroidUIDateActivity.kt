ee950a3a59f0787fb9b1d4a66a5466981ec2a604
Justus Saringer <justus.saringer@check24.com>
2022-03-07 10:24:29 +0100

    added actionbartitle to AndroidUIDateActivity.kt

--- demo/src/main/java/de/check24/demo/features/date/AndroidUIDateActivity.kt
+++ demo/src/main/java/de/check24/demo/features/date/AndroidUIDateActivity.kt
@@ -2,12 +2,14 @@ package de.check24.demo.features.date
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import de.check24.demo.R
 
-class AndroidUIDateActivity : ComponentActivity(){
+class AndroidUIDateActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.date)
+        supportActionBar?.title = "Date"
     }
 }
