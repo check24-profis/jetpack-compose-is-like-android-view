6c06ed7f1d400e4613fe313b2ab8a6f8b4243752
Justus Saringer <justus.saringer@check24.com>
2022-03-07 16:44:38 +0100

    added switch-code to the website

---
+++ code/button/Switch.kt
@@ -0,0 +1,8 @@
+@Composable
+private fun SwitchComposable() {
+    val checkedState = remember { mutableStateOf(true) }
+    Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
+    )
+}
