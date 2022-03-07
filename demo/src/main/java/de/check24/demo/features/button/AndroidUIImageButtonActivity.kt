77984720f4660be712e54dd5e2b46349c4066121
Darius Mertin <darius.mertin@check24.de>
2022-03-07 14:54:45 +0100

    wip

--- demo/src/main/java/de/check24/demo/features/button/AndroidUIImageButtonActivity.kt
+++ demo/src/main/java/de/check24/demo/features/button/AndroidUIImageButtonActivity.kt
@@ -1,11 +1,21 @@
 package de.check24.demo.features.button
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import de.check24.demo.R
 
 class AndroidUIImageButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.image_button)
+        supportActionBar?.title = "ImageButton"
+    }
+
+    fun onImageButtonClick(view: View) {
+        if (view !is ImageButton) return
+
 
     }
 }
