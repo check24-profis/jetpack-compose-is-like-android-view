bf65de84e60d287bd948f2f7a8152233106477f6
Darius Mertin <darius.mertin@check24.de>
2022-03-07 14:32:58 +0100

    Adding files

---
+++ demo/src/main/java/de/check24/demo/features/button/AndroidUIImageButtonActivity.kt
@@ -0,0 +1,11 @@
+package de.check24.demo.features.button
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AndroidUIImageButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ demo/src/main/java/de/check24/demo/features/button/ComposableImageButtonActivity.kt
@@ -0,0 +1,4 @@
+package de.check24.demo.features.button
+
+class ComposableImageButtonActivity {
+}
