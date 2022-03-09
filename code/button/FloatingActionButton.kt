a7818a4328c75406ac308f83f007ea66b411f9c3
Justus Saringer <justus.saringer@check24.com>
2022-03-09 15:32:18 +0100

    adopted the comments, like more fabs and formatting the code

--- code/button/FloatingActionButton.kt
+++ code/button/FloatingActionButton.kt
@@ -11,3 +11,15 @@ private fun FloatingActionButton() {
         )
     }
 }
+
+/* Or you can use an Extended Floating Action Button */
+
+@Composable
+private fun FloatingActionButton() {
+
+    ExtendedFloatingActionButton(
+        text = { Text(text = "Extended FAB with Icon") },
+        onClick = { /*your code*/ },
+        icon = { Icon(Icons.Filled.Add, contentDescription = "Floating Action Button") }
+    )
+}
--- demo/src/main/java/de/check24/demo/features/button/ComposableFloatingActionButton.kt
+++ demo/src/main/java/de/check24/demo/features/button/ComposableFloatingActionButton.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import de.check24.demo.R
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import de.check24.demo.ui.theme.DemoTheme
 
 class ComposableFloatingActionButton : ComponentActivity() {
@@ -29,13 +36,14 @@ class ComposableFloatingActionButton : ComponentActivity() {
                         TopAppBar(
                             title = {
                                 Text(text = "Floating Action Button")
-                            })
+                            }
+                        )
                     }, content = {
                         Box(
                             modifier = Modifier.fillMaxSize(),
                             contentAlignment = Alignment.Center
                         ) {
-                            FloatingActionButton()
+                            FloatingActionButtonExample()
                         }
                     })
             }
@@ -44,16 +52,85 @@ class ComposableFloatingActionButton : ComponentActivity() {
 }
 
 @Composable
-private fun FloatingActionButton() {
+private fun FloatingActionButtonExample() {
 
-    androidx.compose.material.FloatingActionButton(
-        onClick = { /* your code*/ }
-    ) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_baseline_date_range),
-            contentDescription = "Floating Action Button",
-            tint = androidx.compose.ui.graphics.Color.Black
-        )
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Filled.Add, "Floating Action Button")
+            }
+        }) {
+
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+            val (iconFAB, RectangleFAB, ExtendedFABWithIcon, ExtendedFAB, customFAB) = createRefs()
+
+            FloatingActionButton(
+                modifier = Modifier.constrainAs(iconFAB) {
+                    top.linkTo(parent.top, margin = 40.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                onClick = { /*your code*/ }
+            ) {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = "Floating ActionButton"
+                )
+            }
+
+            FloatingActionButton(
+                modifier = Modifier.constrainAs(RectangleFAB) {
+                    top.linkTo(iconFAB.bottom, margin = 40.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                onClick = { /*your code*/ },
+                shape = RectangleShape
+            ) {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = "Floating ActionButton"
+                )
+            }
+
+            ExtendedFloatingActionButton(
+                modifier = Modifier.constrainAs(ExtendedFABWithIcon) {
+                    top.linkTo(RectangleFAB.bottom, margin = 40.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                text = { Text(text = "Extended FAB with Icon") },
+                onClick = { /*your code*/ },
+                icon = { Icon(Icons.Filled.Add, contentDescription = "Floating Action Button") }
+            )
+
+            ExtendedFloatingActionButton(
+                modifier = Modifier.constrainAs(ExtendedFAB) {
+                    top.linkTo(ExtendedFABWithIcon.bottom, margin = 40.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                text = { Text(text = "Extended FAB") },
+                onClick = { /*your code*/ }
+            )
+
+            FloatingActionButton(
+                modifier = Modifier.constrainAs(customFAB) {
+                    top.linkTo(ExtendedFAB.bottom, margin = 40.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                onClick = { /*your code*/ },
+                backgroundColor = Color.DarkGray,
+                contentColor = Color.White
+            ) {
+                Text(text = "Custom FAB")
+            }
+        }
+
+
     }
 }
 
@@ -61,6 +138,6 @@ private fun FloatingActionButton() {
 @Composable
 private fun FloatingActionButtonPreview() {
     DemoTheme {
-        FloatingActionButton()
+        FloatingActionButtonExample()
     }
 }
