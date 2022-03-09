b8f3d977ef45526e5b7fc0230f2c5227a3f466c5
Florian Stallmach <florian.stallmach@check24.com>
2022-03-09 16:55:28 +0100

    add empty activity

---
+++ demo/src/main/java/de/check24/demo/features/password/ComposableListWithDifferentViewsActivity.kt
@@ -0,0 +1,11 @@
+package de.check24.demo.features.password
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ComposableListWithDifferentViewsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
