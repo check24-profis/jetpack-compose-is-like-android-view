344a4501302554ea20835b5358bda2a5fc9a731f
Florian Stallmach <florian.stallmach@check24.com>
2022-03-09 10:16:30 +0100

    remove all surfaces

--- demo/src/main/java/de/check24/demo/features/constraintlayout/barrier/ComposableBarrierActivity.kt
+++ demo/src/main/java/de/check24/demo/features/constraintlayout/barrier/ComposableBarrierActivity.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
@@ -29,21 +27,16 @@ class ComposableBarrierActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(
-                                title = {
-                                    Text(text = "Constraint Layout (Barrier)")
-                                })
-                        }, content = {
-                            ConstraintLayoutBarrier()
-                        }
-                    )
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Constraint Layout (Barrier)")
+                            })
+                    }, content = {
+                        ConstraintLayoutBarrier()
+                    }
+                )
             }
         }
     }
@@ -66,7 +59,7 @@ private fun ConstraintLayoutBarrier() {
                 }
                 .height(200.dp)
                 .wrapContentWidth()
-                    // background color to see why button is aligned the way it is
+                // background color to see why button is aligned the way it is
                 .background(color = Color.Cyan)
         )
 
--- demo/src/main/java/de/check24/demo/features/date/ComposableDateActivity.kt
+++ demo/src/main/java/de/check24/demo/features/date/ComposableDateActivity.kt
@@ -6,22 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -39,12 +37,7 @@ class ComposableDateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Date()
-                }
+                Date()
             }
         }
         supportActionBar?.title = "Date"
@@ -55,7 +48,8 @@ class ComposableDateActivity : AppCompatActivity() {
 fun Date() {
 
     var datePicked: String by remember {
-        mutableStateOf("Date")}
+        mutableStateOf("Date")
+    }
 
     val updatedDate = { date: String ->
         datePicked = date
