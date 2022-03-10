6a259d7283a52ed337eba128ef5952d75434ad99
Justus Saringer <justus.saringer@check24.com>
2022-03-10 17:54:37 +0100

    added bottomappbar in ComposableBottomAppBarActivity.kt

--- demo/src/main/java/de/check24/demo/features/bar/ComposableBottomAppBarActivity.kt
+++ demo/src/main/java/de/check24/demo/features/bar/ComposableBottomAppBarActivity.kt
@@ -3,31 +3,28 @@ package de.check24.demo.features.bar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.ContentAlpha
+import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material.icons.filled.Upload
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.check24.demo.ui.theme.DemoTheme
 
 class ComposableBottomAppBarActivity : ComponentActivity() {
@@ -35,18 +32,7 @@ class ComposableBottomAppBarActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DemoTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Text(text = "Switch")
-                            }
-                        )
-                    }, content = {
-                        BottomAppBarExample()
-                    },
-
-                )
+                BottomAppBarExample()
             }
         }
     }
@@ -55,39 +41,90 @@ class ComposableBottomAppBarActivity : ComponentActivity() {
 @Composable
 private fun BottomAppBarExample() {
 
-    FloatingActionButton(onClick = { /*TODO*/ }) {
-        Icon(Icons.Filled.Add, "")
-    }
-    
-/*    var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf("Songs", "Artists", "Playlists")
+    val selectedItem = remember { mutableStateOf("") }
 
-    BottomNavigation {
-        items.forEachIndexed { index, item ->
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-                label = { Text(item) },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "BottomAppBar")
+                }
             )
-        }
-    }*/
+        },
+        bottomBar = {
+            BottomAppBar(
+                content = {
+                    BottomNavigation(
+                        modifier = Modifier,
+                        elevation = 22.dp
+                    ) {
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(Icons.Filled.Favorite, "")
+                            },
+                            label = { Text(text = "Favorite") },
+                            selected = selectedItem.value == "favorite",
+                            onClick = {
+                                selectedItem.value = "favorite"
+                            },
+                            alwaysShowLabel = false
+                        )
+
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(Icons.Filled.Save, "")
+                            },
+                            label = { Text(text = "Save") },
+                            selected = selectedItem.value == "save",
+                            onClick = {
+                                selectedItem.value = "save"
+                            },
+                            alwaysShowLabel = false
+                        )
+
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(Icons.Filled.Upload, "")
+                            },
 
 
-    BottomAppBar {
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
-            IconButton(onClick = { /*TODO */}) {
-                Icon(Icons.Filled.Menu, "Menu")
+                            label = { Text(text = "Upload") },
+                            selected = selectedItem.value == "upload",
+                            onClick = {
+                                selectedItem.value = "upload"
+                            },
+                            alwaysShowLabel = false
+                        )
+
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(Icons.Filled.Download, "")
+                            },
+                            label = { Text(text = "Download") },
+                            selected = selectedItem.value == "download",
+                            onClick = {
+                                selectedItem.value = "download"
+                            },
+                            alwaysShowLabel = false
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = true,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}
+            ) {
+                Icon(
+                    Icons.Filled.Add,
+                    "Floating Action Button"
+                )
             }
-        }
-        Spacer(Modifier.weight(1f, true))
-        IconButton(onClick = {  /*doSomething()*/  }) {
-            Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
-        }
-        IconButton(onClick = {  /*doSomething()*/ }) {
-            Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
-        }
-    }
+        },
+        content = {}
+    )
 }
 
 @Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
