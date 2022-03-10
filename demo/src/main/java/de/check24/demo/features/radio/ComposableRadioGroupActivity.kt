e2640b684ac83bd52f515f2d719368b91e11eefa
Justus Saringer <justus.saringer@check24.com>
2022-03-10 10:45:09 +0100

    added radio button group to the radio_group.xml.kt

--- demo/src/main/java/de/check24/demo/features/radio/ComposableRadioGroupActivity.kt
+++ demo/src/main/java/de/check24/demo/features/radio/ComposableRadioGroupActivity.kt
@@ -1,9 +1,9 @@
 package de.check24.demo.features.radio
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,8 +19,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
