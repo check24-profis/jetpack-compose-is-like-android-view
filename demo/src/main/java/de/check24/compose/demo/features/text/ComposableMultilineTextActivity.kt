656fcaa82c1a13a5797ce1ed2ca1efa93837b326
Darius Mertin <darius.mertin@check24.de>
2022-03-15 14:19:39 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/text/ComposableMultilineTextActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/text/ComposableMultilineTextActivity.kt
@@ -2,9 +2,18 @@ package de.check24.compose.demo.features.text
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 
 class ComposableMultilineTextActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+
+        }
     }
+}
+
+@Composable
+private fun MultilineText() {
 }
