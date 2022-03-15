7791c7f37b3f0d15562514e66dbaf93ab42589d8
Darius Mertin <darius.mertin@check24.de>
2022-03-15 11:50:14 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/bar/screens/FavoriteFragment.kt
+++ demo/src/main/java/de/check24/compose/demo/features/bar/screens/FavoriteFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import de.check24.demo.R
+import de.check24.compose.demo.R
 
 class FavoriteFragment : Fragment() {
 
