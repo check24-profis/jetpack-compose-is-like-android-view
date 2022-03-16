0486c207bfd6896141278ca1fbd773d42e8cc5ae
Darius Mertin <darius.mertin@check24.de>
2022-03-16 10:18:12 +0100

    Composable implementation

--- demo/src/main/java/de/check24/compose/demo/features/viewpager/ComposableViewPagerActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/viewpager/ComposableViewPagerActivity.kt
@@ -3,9 +3,22 @@ package de.check24.compose.demo.features.viewpager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import de.check24.compose.demo.theme.DemoTheme
@@ -16,22 +29,21 @@ class ComposableViewPagerActivity : ComponentActivity() {
 
         setContent {
             DemoTheme {
-//                Scaffold(
-//                    topBar = {
-//                        TopAppBar(
-//                            title = {
-//                                Text(text = "Multiline Text")
-//                            })
-//                    }, content = {
-//                        Box(
-//                            modifier = Modifier.fillMaxSize(),
-//                            contentAlignment = Alignment.Center
-//                        ) {
-//                            ViewPagerExample()
-//                        }
-//                    }
-//                )
-                ViewPagerExample()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Multiline Text")
+                            })
+                    }, content = {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            ViewPagerExample()
+                        }
+                    }
+                )
             }
         }
     }
@@ -41,7 +53,20 @@ class ComposableViewPagerActivity : ComponentActivity() {
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 private fun ViewPagerExample() {
-    HorizontalPager(count = 3) {
+    HorizontalPager(count = 5) { page ->
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+            elevation = 10.dp
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.size(40.dp)
+            ) {
+                Text(text = "this is page $page")
+            }
+        }
 
     }
 }
