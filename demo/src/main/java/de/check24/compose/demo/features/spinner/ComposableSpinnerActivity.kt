c331516c780913148cb3a3c6a0affabe80013096
Justus Saringer <justus.saringer@check24.com>
2022-03-17 12:57:24 +0100

    adopted comments

--- demo/src/main/java/de/check24/compose/demo/features/spinner/ComposableSpinnerActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/spinner/ComposableSpinnerActivity.kt
@@ -1,13 +1,30 @@
 package de.check24.compose.demo.features.spinner
 
 import android.os.Bundle
-import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import de.check24.compose.demo.theme.DemoTheme
@@ -36,6 +53,46 @@ class ComposableSpinnerActivity : ComponentActivity() {
 @Composable
 private fun SpinnerExample() {
 
+    val itemList = listOf(
+        "Berlin",
+        "Hamburg",
+        "Stuttgart",
+        "München",
+        "Düsseldorf",
+        "Osnabrück"
+    )
+
+    var expanded by remember { mutableStateOf(false) }
+    var cityname by remember { mutableStateOf("select a city") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+        ) {
+            Text(text = cityname, Modifier.clickable { expanded = !expanded })
+            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                itemList.forEach { city ->
+
+                    DropdownMenuItem(
+                        onClick = {
+                            expanded = false
+                            cityname = city
+                        }
+                    ) {
+                        Text(text = city)
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_4)
