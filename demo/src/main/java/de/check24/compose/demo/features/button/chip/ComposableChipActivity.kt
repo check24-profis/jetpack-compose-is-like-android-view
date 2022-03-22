a6e8df59e1808a213e196694f7ba6e17dc6d2c12
Justus Saringer <justus.saringer@check24.com>
2022-03-22 09:30:56 +0100

    designed new chips

--- demo/src/main/java/de/check24/compose/demo/features/button/chip/ComposableChipActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/button/chip/ComposableChipActivity.kt
@@ -3,17 +3,29 @@ package de.check24.compose.demo.features.button.chip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,30 +54,153 @@ class ComposableChipActivity : ComponentActivity() {
     }
 }
 
+/* there is an official implementation of the chip in 1.2.0-alpha02 */
+
 @Composable
 private fun ChipExample() {
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(241, 255, 250)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Chip()
+        ChipWithIcon()
+        ChipSelectableIcon()
+    }
+}
 
-        // through row we get a riddle effect over the Text with padding
-        Row(
-            modifier = Modifier
-                .background(Color.LightGray)
-                .toggleable(
-                    value = false,
-                    onValueChange = { }
-                )
+@Composable
+fun Chip(
+    name: String = "I am a Chip",
+) {
+    var isSelected by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp,
+        shape = CircleShape,
+        color = if (isSelected) MaterialTheme.colors.primary else Color.LightGray,
+    ) {
+        Row(modifier = Modifier
+            .toggleable(
+                value = isSelected,
+                onValueChange = { isSelected = !isSelected }
+            )
         ) {
             Text(
-                text = "I'm a chip",
-                modifier = Modifier.padding(10.dp)
+                text = name,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
             )
         }
     }
 }
+
+@Composable
+fun ChipWithIcon(name: String = "I am a Chip")
+{
+    var isSelected by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp,
+        shape = CircleShape,
+        color = if (isSelected) Color(69, 69, 69) else Color(241, 255, 250),
+        border = BorderStroke(width = 2.dp, Color(69,69,69))
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .toggleable(
+                    value = isSelected,
+                    onValueChange = { isSelected = !isSelected }
+                )
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body2,
+                color = if (isSelected) Color.White else Color.Black,
+                modifier = Modifier.padding(
+                    start = 8.dp,
+                    top = 8.dp,
+                    bottom = 8.dp
+                )
+            )
+            Icon(
+                Icons.Filled.Add,
+                "",
+                tint = if (isSelected) Color.White else Color.Black,
+                modifier = Modifier.padding(
+                    end = 8.dp,
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun ChipSelectableIcon(name: String = "I am a Chip") {
+    var isSelected by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp,
+        shape = CircleShape,
+        color = if (isSelected) Color(69, 69, 69) else Color(241, 255, 250),
+        border = BorderStroke(width = 2.dp, Color(69,69,69))
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .toggleable(
+                    value = isSelected,
+                    onValueChange = { isSelected = !isSelected }
+                )
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body2,
+                color = if (isSelected) Color.White else Color.Black,
+                modifier = Modifier.padding(
+                    start = 8.dp,
+                    top = 8.dp,
+                    bottom = 8.dp
+                )
+            )
+            Icon(
+                Icons.Filled.Add,
+                "",
+                tint = if (isSelected) Color.White else Color.Black,
+                modifier = Modifier.padding(
+                    end = 8.dp,
+                )
+            )
+        }
+    }
+}
+
+/*@Composable
+private fun Chip() {
+    Row(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .toggleable(
+                value = false,
+                onValueChange = { }
+            )
+    ) {
+        Text(
+            text = "I'm a chip",
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}*/
 
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_4)
 @Composable
