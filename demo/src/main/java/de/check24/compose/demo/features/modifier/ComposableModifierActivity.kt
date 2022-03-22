655d826eb08d1ef9f6128865e54766801b91b146
Darius Mertin <darius.mertin@check24.de>
2022-03-22 16:00:15 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/modifier/ComposableModifierActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/modifier/ComposableModifierActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -51,49 +60,95 @@ class ComposableModifierActivity : ComponentActivity() {
 @Composable
 private fun ModifierDemoScreen() {
 
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-        PaddingDemo()
         MarginDemo()
+        Spacer(modifier = Modifier.size(20.dp))
         SizeDemo()
         ClickableDemo()
-        ShadowDemo()
-        BackgroundDemo()
+        Spacer(modifier = Modifier.size(20.dp))
+//        ShadowDemo()
+        Spacer(modifier = Modifier.size(20.dp))
+//        BackgroundDemo()
     }
 }
 
 @Composable
-fun PaddingDemo() {
+private fun MarginDemo() {
+
+    Card(backgroundColor = Color.Blue) {
+
+        Text(
+            text = "padding (red) and margin (blue)",
+            modifier = Modifier
+                .padding(
+                    horizontal = 50.dp,
+                    vertical = 20.dp
+                ) // Margin
+                .background(Color.Red)
+                .padding(20.dp) // Padding
+        )
+    }
+}
+
+@Composable
+private fun SizeDemo() {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        Card(
+            backgroundColor = Color.LightGray,
+            modifier = Modifier.size(70.dp),
+
+            ) {
+
+            Text(
+                text = "size: big"
+            )
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Card(
+            backgroundColor = Color.LightGray,
+            modifier = Modifier.size(50.dp)
+        ) {
+
+            Text(
+                text = "size: small"
+            )
+        }
+    }
+}
+
+@Composable
+private fun ClickableDemo() {
+    var count by remember { mutableStateOf(0) }
     Text(
-        text = "padding",
-        modifier = Modifier.background(Color.Gray).padding(50.dp)
+        text = "Clicked ${count}x",
+        modifier = Modifier
+            .padding(20.dp)
+            .background(Color.LightGray)
+            .clickable {
+                count++
+            }
     )
 }
 
 @Composable
-fun BackgroundDemo() {
+private fun ShadowDemo() {
     TODO("Not yet implemented")
 }
 
 @Composable
-fun ShadowDemo() {
+private fun BackgroundDemo() {
     TODO("Not yet implemented")
 }
 
-@Composable
-fun ClickableDemo() {
-    TODO("Not yet implemented")
-}
-
-@Composable
-fun SizeDemo() {
-    TODO("Not yet implemented")
-}
-
-@Composable
-fun MarginDemo() {
-    TODO("Not yet implemented")
-}
 
 @Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
 @Composable
