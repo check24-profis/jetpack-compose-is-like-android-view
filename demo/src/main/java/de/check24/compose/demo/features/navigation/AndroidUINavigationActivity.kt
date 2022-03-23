a87f9d2e36c8db4568ef8f2b3573089d87468d96
Darius Mertin <darius.mertin@check24.de>
2022-03-23 10:45:04 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/navigation/AndroidUINavigationActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/navigation/AndroidUINavigationActivity.kt
@@ -2,10 +2,12 @@ package de.check24.compose.demo.features.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import de.check24.compose.demo.R
 
 class AndroidUINavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation)
         supportActionBar?.title = "NavHost"
     }
 }
--- demo/src/main/java/de/check24/compose/demo/features/navigation/ComposableNavigationActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/navigation/ComposableNavigationActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -15,6 +16,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
