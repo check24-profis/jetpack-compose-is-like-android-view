c21260d167b34aee636332193af31414a35d3bde
Darius Mertin <darius.mertin@check24.de>
2022-03-23 09:36:45 +0100

    init

---
+++ demo/src/main/java/de/check24/compose/demo/features/navigation/AndroidUINavigationActivity.kt
@@ -0,0 +1,6 @@
+package de.check24.compose.demo.features.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AndroidUINavigationActivity : AppCompatActivity() {
+}
---
+++ demo/src/main/java/de/check24/compose/demo/features/navigation/ComposableNavigationActivity.kt
@@ -0,0 +1,6 @@
+package de.check24.compose.demo.features.navigation
+
+import androidx.activity.ComponentActivity
+
+class ComposableNavigationActivity : ComponentActivity() {
+}
