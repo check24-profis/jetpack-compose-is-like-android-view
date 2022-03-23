4e97d25b3235c1ce3176185019da3cb59e68570f
Darius Mertin <darius.mertin@check24.de>
2022-03-23 10:21:22 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/navigation/ComposableNavigationActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/navigation/ComposableNavigationActivity.kt
@@ -3,10 +3,18 @@ package de.check24.compose.demo.features.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
@@ -27,7 +35,14 @@ class ComposableNavigationActivity : ComponentActivity() {
                                 Text(text = "NavHost")
                             })
                     }, content = {
-                        NavHostDemo()
+
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+
+                            NavHostDemo()
+                        }
                     }
                 )
             }
@@ -40,19 +55,39 @@ class ComposableNavigationActivity : ComponentActivity() {
 private fun NavHostDemo() {
 
     val navController = rememberNavController()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-    NavHost(navController = navController, startDestination = "A") {
-        
-        composable("A") {
-            Text(text = "A", )
+        NavHost(navController = navController, startDestination = "A") {
+
+            composable("A") {
+                Text(text = "A")
+            }
+
+            composable("B") {
+                Text(text = "B")
+            }
+
+            composable("C") {
+                Text(text = "C")
+            }
         }
-        
-        composable("B") {
-            
-        }
-        
-        composable("C") {
-            
+
+        Row {
+
+            Button(onClick = { navController.navigate("A") }) {
+                Text(text = "A")
+            }
+
+            Button(onClick = { navController.navigate("B") }) {
+                Text(text = "B")
+            }
+
+            Button(onClick = { navController.navigate("C") }) {
+                Text(text = "C")
+            }
         }
     }
 }
