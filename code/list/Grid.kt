f7d0547c50eabbed59521b3d7f7c1883898766ca
Darius Mertin <darius.mertin@check24.de>
2022-03-25 09:40:19 +0100

    corrected

--- code/list/Grid.kt
+++ code/list/Grid.kt
@@ -1,55 +1,3 @@
-package de.check24.compose.demo.features.list.grid
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import de.check24.compose.demo.theme.DemoTheme
-import kotlin.math.min
-
-class ComposableGridActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            DemoTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Text(text = "Grid")
-                            })
-                    }, content = {
-                        Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            GridExample()
-                        }
-                    }
-                )
-            }
-        }
-    }
-}
-
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GridExample() {
@@ -164,12 +112,4 @@ fun getHeights(
     }
 
     return result
-}
-
-@Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
-@Composable
-fun GridExamplePreview() {
-    DemoTheme {
-        GridExample()
-    }
 }
