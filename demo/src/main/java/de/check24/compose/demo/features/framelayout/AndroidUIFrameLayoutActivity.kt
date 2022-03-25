0e0458cf1eb08d15d933629132e932cbc9ca855c
Darius Mertin <darius.mertin@check24.de>
2022-03-25 11:39:40 +0100

    wip

--- demo/src/main/java/de/check24/compose/demo/features/framelayout/AndroidUIFrameLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/framelayout/AndroidUIFrameLayoutActivity.kt
@@ -2,10 +2,12 @@ package de.check24.compose.demo.features.framelayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import de.check24.compose.demo.R
 
 class AndroidUIFrameLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.frame_layout)
         supportActionBar?.title = "Frame Layout"
     }
 }
