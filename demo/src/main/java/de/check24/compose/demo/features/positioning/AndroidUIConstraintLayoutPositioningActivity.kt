ff6b497b8e5e29796031c95f0cc66823e92202aa
Justus Saringer <justus.saringer@check24.com>
2022-03-25 12:04:04 +0100

    added added chain constraint constraint to xml

--- demo/src/main/java/de/check24/compose/demo/features/positioning/AndroidUIConstraintLayoutPositioningActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/positioning/AndroidUIConstraintLayoutPositioningActivity.kt
@@ -7,14 +7,9 @@ import de.check24.compose.demo.databinding.ConstraintLayoutPositioningBinding
 
 class AndroidUIConstraintLayoutPositioningActivity : AppCompatActivity(){
 
-    private var binding: ConstraintLayoutPositioningBinding? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ConstraintLayoutPositioningBinding.inflate(layoutInflater)
-        if (binding == null) throw Exception("ConstraintLayoutPositioningBinding could not be bounded")
-        setContentView(binding?.root)
-//        setContentView(R.layout.constraint_layout_positioning)
+        setContentView(R.layout.constraint_layout_positioning)
         supportActionBar?.title = "ConstraintLayout"
     }
 }
