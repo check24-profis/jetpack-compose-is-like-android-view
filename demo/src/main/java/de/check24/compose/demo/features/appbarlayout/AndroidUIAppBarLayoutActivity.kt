8240b748565ea883ae24b52c9f08d15d8f435fb0
Darius Mertin <darius.mertin@check24.de>
2022-03-28 10:47:04 +0200

    implementation

--- demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
@@ -2,11 +2,12 @@ package de.check24.compose.demo.features.appbarlayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import de.check24.compose.demo.R
 
 class AndroidUIAppBarLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        supportActionBar?.title = "App Bar Layout"
+        supportActionBar?.hide()
+        setContentView(R.layout.app_bar_layout)
     }
 }
--- demo/src/main/java/de/check24/compose/demo/features/appbarlayout/ComposableAppBarLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/appbarlayout/ComposableAppBarLayoutActivity.kt
@@ -2,10 +2,39 @@ package de.check24.compose.demo.features.appbarlayout
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import de.check24.compose.demo.theme.DemoTheme
 
 class ComposableAppBarLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
+        setContent {
+            DemoTheme {
+                AppBarExample()
+            }
+        }
+    }
+}
+
+@Composable
+private fun AppBarExample() {
+    TopAppBar {
+        Text(
+            text = "App Bar Layout"
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
+@Composable
+private fun AppBarExamplePreview() {
+    DemoTheme {
+        AppBarExample()
     }
 }
