ab1f000efba2da81001a7ef7673d8193d75e00f6
Darius Mertin <darius.mertin@check24.de>
2022-03-29 11:34:51 +0200

    changed binding to lateinit

--- demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
@@ -3,10 +3,11 @@ package de.check24.compose.demo.features.appbarlayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import de.check24.compose.demo.databinding.AppBarLayoutBinding
+import java.lang.NullPointerException
 
 class AndroidUIAppBarLayoutActivity : AppCompatActivity() {
 
-    private var binding: AppBarLayoutBinding? = null
+    private lateinit var binding: AppBarLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +15,8 @@ class AndroidUIAppBarLayoutActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         binding = AppBarLayoutBinding.inflate(layoutInflater)
-        binding?.toolbar?.navigationIcon?.setTint(0xFFFFFFFF.toInt())
+        binding.toolbar.navigationIcon?.setTint(0xFFFFFFFF.toInt())
 
-        setContentView(binding?.root)
+        setContentView(binding.root)
     }
 }
