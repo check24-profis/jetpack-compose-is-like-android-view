d066da32bee43e0884787dd05d37409d56587626
Darius Mertin <darius.mertin@check24.de>
2022-03-29 10:11:47 +0200

    removed redundant box

--- code/list/Grid.kt
+++ code/list/Grid.kt
@@ -9,20 +9,20 @@ fun GridExample() {
         Color(0xFF3700B3)
     )
 
-    Box(Modifier.size(200.dp)) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        modifier = Modifier.size(200.dp)
+    ) {
 
-        LazyVerticalGrid(cells = GridCells.Fixed(2)) {
+        (0..3).forEach {
 
-            (0..3).forEach {
+            item {
 
-                item {
-
-                    Box(
-                        modifier = Modifier
-                            .size(100.dp)
-                            .background(colorList[it])
-                    )
-                }
+                Box(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .background(colorList[it])
+                )
             }
         }
     }
