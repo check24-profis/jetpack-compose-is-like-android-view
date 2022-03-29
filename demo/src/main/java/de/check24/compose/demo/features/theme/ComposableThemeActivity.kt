9966ab3373354792ec5f39a4de3c95bed50d0f63
Darius Mertin <darius.mertin@check24.de>
2022-03-29 16:16:55 +0200

    added typography to democomposable

--- demo/src/main/java/de/check24/compose/demo/features/theme/ComposableThemeActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/theme/ComposableThemeActivity.kt
@@ -3,6 +3,7 @@ package de.check24.compose.demo.features.theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,7 +20,13 @@ import androidx.compose.material.icons.sharp.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import de.check24.compose.demo.theme.DemoTheme
 
 
 class ComposableThemeActivity : ComponentActivity() {
@@ -31,7 +38,6 @@ class ComposableThemeActivity : ComponentActivity() {
         }
     }
 }
-
 
 
 @Composable
@@ -52,8 +58,15 @@ private fun ExampleComposable() {
             topBar = {
                 TopAppBar(
                     title = {
-                        Text(text = "Text View")
-                    })
+                        Text(
+                            text = "Text View",
+                            style = TextStyle(
+                                color = Color.White,
+                                fontSize = 20.sp
+                            )
+                        )
+                    }
+                )
             }, content = {
 
                 Box(
@@ -62,11 +75,27 @@ private fun ExampleComposable() {
                 ) {
 
                     Card {
-                        Column {
+                        Column(
+                            modifier = Modifier.padding(50.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
 
-                            Text(text = "this is some text")
+                            Text(text = "h1", style = MaterialTheme.typography.h1)
+                            Text(text = "h2", style = MaterialTheme.typography.h1)
+                            Text(text = "h3", style = MaterialTheme.typography.h1)
+                            Text(text = "h4", style = MaterialTheme.typography.h1)
+                            Text(text = "h5", style = MaterialTheme.typography.h1)
+                            Text(text = "h6", style = MaterialTheme.typography.h1)
+                            Text(text = "s1", style = MaterialTheme.typography.subtitle1)
+                            Text(text = "s2", style = MaterialTheme.typography.subtitle2)
+                            Text(text = "b1", style = MaterialTheme.typography.body1)
+                            Text(text = "b2", style = MaterialTheme.typography.body2)
+                            Text(text = "button", style = MaterialTheme.typography.button)
+                            Text(text = "caption", style = MaterialTheme.typography.caption)
+                            Text(text = "overline", style = MaterialTheme.typography.overline)
 
-                            Button(onClick = {}, modifier = Modifier.padding(100.dp)) {
+                            Button(onClick = {}) {
                                 Icon(Icons.Sharp.Menu, contentDescription = "theme demo button")
                             }
                         }
@@ -74,5 +103,13 @@ private fun ExampleComposable() {
                 }
             }
         )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.NEXUS_6, showSystemUi = true)
+@Composable
+private fun ThemePreview() {
+    DemoTheme {
+        ExampleComposable()
     }
 }
