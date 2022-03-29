7513e9bfb6d28dbeffa131b58df49841b65e1c43
Darius Mertin <darius.mertin@check24.de>
2022-03-29 10:58:07 +0200

    replaced constraintlayout with toolbar

--- demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/appbarlayout/AndroidUIAppBarLayoutActivity.kt
@@ -2,12 +2,22 @@ package de.check24.compose.demo.features.appbarlayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
 import de.check24.compose.demo.R
+import de.check24.compose.demo.databinding.AppBarLayoutBinding
 
 class AndroidUIAppBarLayoutActivity : AppCompatActivity() {
+
+    private var binding: AppBarLayoutBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         supportActionBar?.hide()
-        setContentView(R.layout.app_bar_layout)
+
+        binding = AppBarLayoutBinding.inflate(layoutInflater)
+        binding?.toolbar?.navigationIcon?.setTint(0xFFFFFFFF.toInt())
+
+        setContentView(binding?.root)
     }
 }
