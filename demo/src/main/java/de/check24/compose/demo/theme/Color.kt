e855f4be70df09d49a002fa95df9fa06ca83f1ba
Justus Saringer <justus.saringer@check24.com>
2022-03-29 10:13:07 +0200

    added colors

--- demo/src/main/java/de/check24/compose/demo/theme/Color.kt
+++ demo/src/main/java/de/check24/compose/demo/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Orange200 = Color(0xFFFFCC80)
+val Green200 = Color(0xFFA5D6A7)
+val Blue200 = Color(0xFF90CAF9)
+val Red200 = Color(0xFFEF9A9A)
