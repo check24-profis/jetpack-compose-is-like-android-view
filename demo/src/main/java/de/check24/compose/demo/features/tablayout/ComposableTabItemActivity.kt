964b688fd6776497249f2fb16f8a3d935896f680
Justus Saringer <justus.saringer@check24.com>
2022-03-30 15:59:13 +0200

    added TabItems to compose

--- demo/src/main/java/de/check24/compose/demo/features/tablayout/ComposableTabItemActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/tablayout/ComposableTabItemActivity.kt
@@ -3,10 +3,28 @@ package de.check24.compose.demo.features.tablayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import de.check24.compose.demo.theme.DemoTheme
@@ -35,7 +53,89 @@ class ComposableTabItemActivity : ComponentActivity() {
 
 @Composable
 private fun TabItemExample() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
 
+        ) {
+            TextTabs()
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconTabs()
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconAndTextTabs()
+        }
+    }
+}
+
+@Composable
+private fun IconTabs() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabData = listOf(
+        Icons.Filled.Home,
+        Icons.Filled.ShoppingCart,
+        Icons.Filled.AccountBox,
+        Icons.Filled.Settings,
+    )
+    TabRow(selectedTabIndex = tabIndex) {
+        tabData.forEachIndexed { index, icon ->
+            Tab(selected = tabIndex == index, onClick = {
+                tabIndex = index
+            }, icon = {
+                Icon(imageVector = icon, contentDescription = null)
+            })
+        }
+    }
+}
+
+@Composable
+private fun TextTabs() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabData = listOf(
+        "MUSIC",
+        "MARKET",
+        "FILMS",
+        "BOOKS",
+    )
+    TabRow(selectedTabIndex = tabIndex) {
+        tabData.forEachIndexed { index, text ->
+            Tab(selected = tabIndex == index, onClick = {
+                tabIndex = index
+            }, text = {
+                Text(text = text)
+            })
+        }
+    }
+}
+
+@Composable
+private fun IconAndTextTabs() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabData = listOf(
+        "MUSIC" to Icons.Filled.Home,
+        "MARKET" to Icons.Filled.ShoppingCart,
+        "FILMS" to Icons.Filled.AccountBox,
+        "BOOKS" to Icons.Filled.Settings,
+    )
+    TabRow(selectedTabIndex = tabIndex) {
+        tabData.forEachIndexed { index, pair ->
+            Tab(selected = tabIndex == index, onClick = {
+                tabIndex = index
+            }, text = {
+                Text(text = pair.first)
+            }, icon = {
+                Icon(imageVector = pair.second, contentDescription = null)
+            })
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_4)
