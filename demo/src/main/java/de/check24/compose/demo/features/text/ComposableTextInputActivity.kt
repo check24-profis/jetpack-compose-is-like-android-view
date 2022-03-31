afbc0b8a89f987f61868da3403e36e3995689003
Justus Saringer <justus.saringer@check24.com>
2022-03-31 13:30:08 +0200

    added Text Input to xml and compose

--- demo/src/main/java/de/check24/compose/demo/features/text/ComposableTextInputActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/text/ComposableTextInputActivity.kt
@@ -8,9 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import de.check24.compose.demo.theme.DemoTheme
@@ -23,7 +29,7 @@ class ComposableTextInputActivity : ComponentActivity() {
                 Scaffold(
                     topBar = {
                         TopAppBar(
-                            title = { Text(text = "") }
+                            title = { Text(text = "Text Input") }
                         )
                     }
                 ) {
@@ -42,6 +48,15 @@ class ComposableTextInputActivity : ComponentActivity() {
 @Composable
 private fun TextInputExample() {
 
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text("hint") }
+    )
 }
 
 @Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_4)
