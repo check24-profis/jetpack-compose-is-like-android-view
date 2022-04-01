006c8e0b45ecd85bea8fc194888f6d29b97ac1f5
Justus Saringer <justus.saringer@check24.com>
2022-04-01 14:24:53 +0200

    implementing stateless example *work in progress*

---
+++ demo/src/main/java/de/check24/compose/demo/features/stateless/ComposableStatelessActivity.kt
@@ -0,0 +1,58 @@
+package de.check24.compose.demo.features.stateless
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import de.check24.compose.demo.theme.DemoTheme
+
+class ComposableStatelessActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DemoTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = { Text(text = "Stateless") })
+                    }
+                ) {
+                    StatelessExample()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun StatelessExample(){
+
+    var isClicked by remember{mutableStateOf(false)}
+
+    MySwitch(isClicked = isClicked)
+}
+
+@Composable
+private fun MySwitch(isClicked: Boolean, onToggle : () -> Unit) {
+    Switch(
+        checked = isClicked,
+        onCheckedChange = {onToggle}
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_4)
+@Composable
+private fun StatelessExamplePreview() {
+    DemoTheme {
+        StatelessExample()
+    }
+}
