5d022175f4135f6e44433feb45dd07396381e557
Justus Saringer <justus.saringer@check24.com>
2022-04-04 16:46:04 +0200

    added compose viewmodel correctly

--- demo/src/main/java/de/check24/compose/demo/features/viewmodel/ComposableViewModelActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/viewmodel/ComposableViewModelActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -25,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import de.check24.compose.demo.theme.Blue200
 import de.check24.compose.demo.theme.DemoTheme
 import de.check24.compose.demo.theme.Green200
@@ -47,45 +49,39 @@ class ComposableViewModelActivity : ComponentActivity() {
             }
         }
     }
-    @Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_4)
-    @Composable
-    private fun ViewModelExamplePreview() {
-        DemoTheme {
-            MyScreen(myViewModel = myViewModel)
-        }
-    }
 }
+/* the composable way */
 
 // the ViewModel manages the states and the events
 // it also has access to the business logic and lives longer than the composable
-class MyViewModel : ViewModel() {
+class MyViewModel : ViewModel(), IMyViewModel {
     // states
-    var text by mutableStateOf("")
-    var isClicked by mutableStateOf(false)
+    override var text = mutableStateOf("")
+    override var isClicked = mutableStateOf(false)
 
     // events
-    fun onTextChanged(newText: String) {
-        text = newText
+    override fun onTextChanged(newText: String) {
+        text.value = newText
     }
-    fun onToggle(clicked: Boolean) {
-        isClicked = clicked
+    override fun onToggle(clicked: Boolean) {
+        isClicked.value = clicked
     }
 }
 
 @Composable
-fun MyScreen(myViewModel: MyViewModel) {
+fun MyScreen(myViewModel: MyViewModel = viewModel()) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         OutlinedTextField(
-            value = myViewModel.text,
+            value = myViewModel.text.value,
             onValueChange = { myViewModel.onTextChanged(it) },
         )
 
         Switch(
-            checked = myViewModel.isClicked,
+            checked = myViewModel.isClicked.value,
             onCheckedChange = { myViewModel.onToggle(it) }
         )
 
@@ -93,9 +89,67 @@ fun MyScreen(myViewModel: MyViewModel) {
             modifier = Modifier
                 .size(100.dp)
                 .background(
-                    if (myViewModel.isClicked) Green200 else Blue200
+                    if (myViewModel.isClicked.value) Green200 else Blue200
                 )
         )
     }
 }
 
+/* with this way the preview works */
+interface IMyViewModel {
+    val text: MutableState<String>
+    val isClicked : MutableState<Boolean>
+
+    fun onTextChanged(newText: String)
+    fun onToggle(clicked: Boolean)
+}
+
+@Composable
+//fun MyScreen(myViewModel: MyViewModel = viewModel()) {
+fun MyScreen(myViewModel: IMyViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = myViewModel.text.value,
+            onValueChange = { myViewModel.onTextChanged(it) },
+        )
+
+        Switch(
+            checked = myViewModel.isClicked.value,
+            onCheckedChange = { myViewModel.onToggle(it) }
+        )
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(
+                    if (myViewModel.isClicked.value) Green200 else Blue200
+                )
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_4)
+@Composable
+private fun ViewModelExamplePreview() {
+    DemoTheme {
+        val myViewModel = object:IMyViewModel{
+            override val text: MutableState<String>
+                get() = mutableStateOf("Hello World")
+            override val isClicked: MutableState<Boolean>
+                get() = mutableStateOf(false)
+
+            override fun onTextChanged(newText: String) {
+                text.value = newText
+            }
+
+            override fun onToggle(clicked: Boolean) {
+                isClicked.value = clicked
+            }
+        }
+        MyScreen(myViewModel = myViewModel)
+    }
+}
