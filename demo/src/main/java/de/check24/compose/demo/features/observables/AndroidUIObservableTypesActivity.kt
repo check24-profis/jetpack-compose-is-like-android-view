7ba029bf4b8095806743590b488a63a7a688e034
Justus Saringer <justus.saringer@check24.com>
2022-04-06 15:11:14 +0200

    added observable types to xml

--- demo/src/main/java/de/check24/compose/demo/features/observables/AndroidUIObservableTypesActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/observables/AndroidUIObservableTypesActivity.kt
@@ -1,13 +1,44 @@
 package de.check24.compose.demo.features.observables
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import de.check24.compose.demo.R
+import de.check24.compose.demo.databinding.ObservableTypesBinding
 
 class AndroidUIObservableTypesActivity:AppCompatActivity() {
+
+    private var binding: ObservableTypesBinding? = null
+    private var viewModel = ObservableViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.observable_types)
+        binding = ObservableTypesBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
         supportActionBar?.title = "Observable types"
+
+        binding?.observableSwitch?.setOnClickListener {
+            viewModel.onClick()
+            setBackgroundColor(binding?.viewBox, viewModel.isClicked)
+        }
+    }
+
+    private fun setBackgroundColor(viewBox: View?, clicked: MutableLiveData<Boolean>) {
+        if (clicked.value == true) {
+            viewBox?.setBackgroundResource(R.color.green_200)
+        } else {
+            viewBox?.setBackgroundResource(R.color.blue_200)
+        }
+    }
+}
+
+class ObservableViewModel: ViewModel() {
+
+    val isClicked: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
+
+    fun onClick() {
+        isClicked.value = !(requireNotNull(isClicked.value))
     }
 }
