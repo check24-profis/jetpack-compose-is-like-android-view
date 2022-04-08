dc75b1b66be069ffc1f5bd55cc0de74b55a190ff
Justus Saringer <justus.saringer@check24.com>
2022-04-08 09:23:08 +0200

    added helper layout in xml

--- demo/src/main/java/de/check24/compose/demo/features/helperlayout/AndroidUIHelperLayoutActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/helperlayout/AndroidUIHelperLayoutActivity.kt
@@ -1,13 +1,40 @@
 package de.check24.compose.demo.features.helperlayout
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import de.check24.compose.demo.R
+import de.check24.compose.demo.databinding.HelperLayoutBinding
 
-class AndroidUIHelperLayoutActivity: AppCompatActivity() {
+class AndroidUIHelperLayoutActivity : AppCompatActivity() {
+
+    private var binding: HelperLayoutBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.helper_layout)
+        binding = HelperLayoutBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
         supportActionBar?.title = "Helper (Layout)"
+
+        binding?.rotateButton?.setOnClickListener {
+            ValueAnimator.ofFloat(0F, 360F)
+                .apply {
+                    addUpdateListener { animator ->
+                        binding?.allViews?.rotation = animator.animatedValue as Float
+                    }
+                    duration = 2000
+                    start()
+                }
+        }
+
+        binding?.rotateRedAndGreenButton?.setOnClickListener {
+            ValueAnimator.ofFloat(0F, 360F)
+                .apply {
+                    addUpdateListener { animator ->
+                        binding?.viewRedAndGreen?.rotation = animator.animatedValue as Float
+                    }
+                    duration = 2000
+                    start()
+                }
+        }
     }
 }
