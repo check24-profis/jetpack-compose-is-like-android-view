ebcc9691163360f56633cf8c6e0061c43c16f6f4
Justus Saringer <justus.saringer@check24.com>
2022-04-12 17:34:14 +0200

    removed Fragment and switched everything to the Activity

--- demo/src/main/java/de/check24/compose/demo/features/events/AndroidUIEventsActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/events/AndroidUIEventsActivity.kt
@@ -1,20 +1,46 @@
 package de.check24.compose.demo.features.events
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import de.check24.compose.demo.R
+import de.check24.compose.demo.databinding.EventLayoutBinding
 
 class AndroidUIEventsActivity: AppCompatActivity() {
+
+    lateinit var binding: EventLayoutBinding
+    private val viewModel: EventViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        setContentView(R.layout.events_fragment)
+        binding = EventLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.title = "Events"
 
-        val eventFragment = EventFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.events_container, eventFragment)
-            .commit()
+        binding.apply {
+
+            addButton.setOnClickListener {
+                viewModel.plusOne()
+                textView.text = viewModel.number.toString()
+            }
+
+            subtractButton.setOnClickListener {
+                viewModel.minusOne()
+                textView.text = viewModel.number.toString()
+            }
+        }
+    }
+}
+
+class EventViewModel : ViewModel() {
+    var number = 0
+
+    fun plusOne() {
+        number++
+    }
+
+    fun minusOne(){
+        number--
     }
 }
