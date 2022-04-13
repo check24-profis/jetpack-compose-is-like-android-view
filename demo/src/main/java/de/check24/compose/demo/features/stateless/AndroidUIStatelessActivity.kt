97f0a386a87d58c3502d09f129d24e7d0351e8bd
Justus Saringer <justus.saringer@check24.com>
2022-04-13 17:10:06 +0200

    added saved instance

--- demo/src/main/java/de/check24/compose/demo/features/stateless/AndroidUIStatelessActivity.kt
+++ demo/src/main/java/de/check24/compose/demo/features/stateless/AndroidUIStatelessActivity.kt
@@ -1,6 +1,8 @@
 package de.check24.compose.demo.features.stateless
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import de.check24.compose.demo.R
 import de.check24.compose.demo.databinding.StatefulBinding
@@ -17,23 +19,29 @@ class AndroidUIStatelessActivity : AppCompatActivity() {
         supportActionBar?.title = "Stateless"
 
         binding.switchExample.setOnClickListener {
-            if (isChecked) {
-                binding.box.setBackgroundColor(resources.getColor(R.color.blue_200))
-            } else {
-                binding.box.setBackgroundColor(resources.getColor(R.color.green_200))
-            }
-            isChecked = !isChecked
+            setColorOnValueChanged()
         }
+    }
+
+    private fun setColorOnValueChanged() {
+        if (isChecked) {
+            binding.box.setBackgroundColor(resources.getColor(R.color.blue_200))
+        } else {
+            binding.box.setBackgroundColor(resources.getColor(R.color.green_200))
+        }
+        isChecked = !isChecked
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean("state", false)
+        Log.i(TAG, "onSaveInstanceState")
+        outState.putBoolean("state", true)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         isChecked = savedInstanceState.getBoolean("state")
+        setColorOnValueChanged()
     }
 }
 
